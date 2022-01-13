91362bbfb6504b94b0bd76567c0ac544872c261e
Gero <gersonhoa@gmail.com>
2022-01-13 19:02:16 -0300

    rq 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,68 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      invalMiLen: true,
+      loginName: '',
+      isLoading: false,
+      ready: false,
+    };
+    this.callCreateUser = this.callCreateUser.bind(this);
+  }
+
+  async callCreateUser() {
+    const { loginName } = this.state;
+    this.setState({ isLoading: true });
+    await createUser({ name: loginName });
+    this.setState({ ready: true });
+  }
+
   render() {
-    return <div data-testid="page-login"> </div>;
+    const { invalMiLen, isLoading, ready } = this.state;
+    return (
+      <div data-testid="page-login">
+        <form>
+          {!ready ? (
+            <label htmlFor="login">
+              <input
+                data-testid="login-name-input"
+                onChange={ ({ target }) => {
+                  const minLen = 3;
+                  if (target.value.length >= minLen) {
+                    this.setState({
+                      invalMiLen: false,
+                      loginName: target.value,
+                    });
+                  } else {
+                    this.setState({
+                      invalMiLen: true,
+                      loginName: target.value });
+                  }
+                } }
+              />
+              <button
+                data-testid="login-submit-button"
+                type="button"
+                disabled={ invalMiLen }
+                onClick={ this.callCreateUser }
+              >
+                Entrar
+              </button>
+            </label>
+          ) : (
+            <Redirect to="/search" />
+          )}
+
+          {isLoading
+            ? <Loading />
+            : null }
+        </form>
+      </div>);
   }
 }
 
