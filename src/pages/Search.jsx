a5ad3e7b01bfdc3b8e2cb6dda3fc2ea3ca71e2f4
Gero <gersonhoa@gmail.com>
2022-01-14 09:45:47 -0300

    rq 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,48 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      invalMiLen: true,
+      // bandName: '',
+      // isLoading: false,
+      // ready: false,
+    };
+  }
+
   render() {
+    const { invalMiLen } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            data-testid="search-artist-input"
+            onChange={ ({ target }) => {
+              const minLen = 2;
+              if (target.value.length >= minLen) {
+                this.setState({
+                  invalMiLen: false,
+                  // bandName: target.value,
+                });
+              } else {
+                this.setState({
+                  invalMiLen: true,
+                  // bandName: target.value
+                });
+              }
+            } }
+          />
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ invalMiLen }
+            onClick={ this.callCreateUser }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
