964a47ef33bf8fc35ac9026437474138c30dd10c
Gero <gersonhoa@gmail.com>
2022-01-14 09:30:00 -0300

    rq 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -32,6 +33,9 @@ export default class Header extends Component {
     return (
       <header data-testid="header-component">
         <h1>
+          <Link to="/search" data-testid="link-to-search"> Search </Link>
+          <Link to="/favorites" data-testid="link-to-favorites"> Favorites </Link>
+          <Link to="/profile" data-testid="link-to-profile"> Profile </Link>
           <p data-testid="header-user-name"> {
             isLoading
               ? username
